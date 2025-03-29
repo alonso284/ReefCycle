8dee26fb9977f650cbe955be9c3088e9e8d22cc6
Alonso Huerta <alonso12_284@outlook.com>
2025-03-28 20:56:11 -0600

    custom reefy

---
+++ Shared/Model/CustomReefy.swift
@@ -0,0 +1,8 @@
+//
+//  CustomReefy.swift
+//  ReefCycle
+//
+//  Created by Alonso Huerta on 28/03/25.
+//
+
+import Foundation
---
+++ Shared/View/ReefyView.swift
@@ -0,0 +1,18 @@
+//
+//  ReefyView.swift
+//  ReefCycle
+//
+//  Created by Alonso Huerta on 28/03/25.
+//
+
+import SwiftUI
+
+struct ReefyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReefyView()
+}
